c60eaafa444caa6441e35e42801af6839eba535d
개앞맵시(이복연) <wegra.lee@gmail.com>
2025-01-17 18:18:43 +0900

    Update train_neuralnet.py
    실행 위치 상관없이 동작하도록 개선

--- ch04/train_neuralnet.py
+++ ch04/train_neuralnet.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+
 import numpy as np
 import matplotlib.pyplot as plt
 from dataset.mnist import load_mnist
