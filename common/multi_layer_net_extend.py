ba428c361bf4192732d30bd9bab575d71291b1ea
개앞맵시(이복연) <wegra.lee@gmail.com>
2025-01-17 19:16:50 +0900

    Update multi_layer_net_extend.py
    실행 위치 상관없이 동작하도록 개선

--- common/multi_layer_net_extend.py
+++ common/multi_layer_net_extend.py
@@ -1,6 +1,6 @@
 # coding: utf-8
-import sys, os
-sys.path.append(os.pardir) # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
 import numpy as np
 from collections import OrderedDict
 from common.layers import *
