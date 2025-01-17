4a7992cc59922efd79873d4c2cce919efafdfa3f
개앞맵시(이복연) <wegra.lee@gmail.com>
2025-01-17 18:01:39 +0900

    Update mnist_show.py
    실행 위치 상관없이 동작하도록 개선

--- ch03/mnist_show.py
+++ ch03/mnist_show.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+sys.path.append(os.path.join(os.path.dirname(__file__), '..')) # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+
 import numpy as np
 from dataset.mnist import load_mnist
 from PIL import Image
