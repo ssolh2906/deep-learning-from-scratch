4154aa1840c27b37fe3e3347e55942748ce1b793
개앞맵시(이복연) <wegra.lee@gmail.com>
2025-01-17 18:32:00 +0900

    Update train_neuralnet.py
    실행 위치 상관없이 동작하도록 개선

--- ch05/train_neuralnet.py
+++ ch05/train_neuralnet.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 import numpy as np
 from dataset.mnist import load_mnist
