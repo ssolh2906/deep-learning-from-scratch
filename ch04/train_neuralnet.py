f534f2337a2c6c6ecf7fc4b099f7e8da9b26280d
개앞맵시(이복연) <wegra.lee@gmail.com>
2025-01-17 18:30:21 +0900

    Update train_neuralnet.py
    '시험 데이터로 평가하기' 코드를 train_neuralnet_2.py로 분리
    - 4.5.2절의 코드
    - '[그림 4-11] 손실 함수 값의 추이'의 왼쪽 그래프 출력

--- ch04/train_neuralnet.py
+++ ch04/train_neuralnet.py
@@ -10,7 +10,7 @@ from two_layer_net import TwoLayerNet
 # 데이터 읽기
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
-network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+train_loss_list = []
 
 # 하이퍼파라미터
 iters_num = 10000  # 반복 횟수를 적절히 설정한다.
@@ -18,12 +18,7 @@ train_size = x_train.shape[0]
 batch_size = 100   # 미니배치 크기
 learning_rate = 0.1
 
-train_loss_list = []
-train_acc_list = []
-test_acc_list = []
-
-# 1에폭당 반복 수
-iter_per_epoch = max(train_size / batch_size, 1)
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
 for i in range(iters_num):
     # 미니배치 획득
@@ -42,22 +37,12 @@ for i in range(iters_num):
     # 학습 경과 기록
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
-    
-    # 1에폭당 정확도 계산
-    if i % iter_per_epoch == 0:
-        train_acc = network.accuracy(x_train, t_train)
-        test_acc = network.accuracy(x_test, t_test)
-        train_acc_list.append(train_acc)
-        test_acc_list.append(test_acc)
-        print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
 
 # 그래프 그리기
-markers = {'train': 'o', 'test': 's'}
-x = np.arange(len(train_acc_list))
-plt.plot(x, train_acc_list, label='train acc')
-plt.plot(x, test_acc_list, label='test acc', linestyle='--')
-plt.xlabel("epochs")
-plt.ylabel("accuracy")
-plt.ylim(0, 1.0)
-plt.legend(loc='lower right')
+x = np.arange(len(train_loss_list))
+plt.plot(x, train_loss_list)
+plt.xlabel("iteration")
+plt.ylabel("loss")
+plt.ylim(0, 9)
+plt.xlim(0, 10000)
 plt.show()
