ac5b97aeca56413e58f13fe9c2055bde3d40a4a1
Sol_P_T <91825053+ssolh2906@users.noreply.github.com>
2025-11-08 10:38:36 -0800

    Minor reformat

--- bioinformatics_examples/ch03/build_pwm.py
+++ bioinformatics_examples/ch03/build_pwm.py
@@ -1,5 +1,5 @@
-'''
+"""
 build_pwm.py
 
 Generate sample_weight_pwm.pkl (W,b like 'sample_weight.pkl' in original example)
-'''
+"""
--- bioinformatics_examples/ch03/load_data.py
+++ bioinformatics_examples/ch03/load_data.py
@@ -2,10 +2,10 @@ from ucimlrepo import fetch_ucirepo
 
 
 def _load_raw_from_ucirepo():
-    '''
+    """
     Fetch UCI Splice-junction Gene Sequence dataset using ucimlrepo package. (id = 69).
     Return list_x., list_t
-    '''
+    """
 
     # fetch dataset
     molecular_biology_splice_junction_gene_sequences = fetch_ucirepo(id=69)
@@ -20,4 +20,4 @@ def _load_raw_from_ucirepo():
     # variable information
     print(molecular_biology_splice_junction_gene_sequences.variables)
 
-    return (list_x, list_t)
+    return list_x, list_t
